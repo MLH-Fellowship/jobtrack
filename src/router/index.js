13276a8f2b5e3159a4ad643132068945f58aa342
Larson Carter <larsoncarter@blazermail.net>
2020-07-14 11:32:36 -0500

    Update Routes + rename Scheme
    I updated the routes to support lazy loading.
    
    I also renamed the Views to Just the name

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
   }
 ]
 
