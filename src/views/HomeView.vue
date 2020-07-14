c9550277f7b3ddf5c692bf4f114430056188f738
Larson Carter <larsoncarter@blazermail.net>
2020-07-14 09:39:57 -0500

    Cleanup Default
    I replaced all of the defaults that were included in VueJS. I also removed some of the non essential parts of code to get ready to make the UI/UX.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HomeComponent/>
+  </div>
+</template>
+
+<script>
+import HomeComponent from '@/components/HomeComponent.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeComponent
+  }
+}
+</script>
