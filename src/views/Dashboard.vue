19fe7026a91041d2830ef132407c44a94cd8fe87
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2020-07-15 19:08:21 +0000

    Restructur views

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <router-link to="/logout">Log out</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Dashboard',
+  data: () => {
+    return {
+      // sidebarOpen: false
+    }
+  },
+  methods: {
+    // Log the user in
+    logout () {
+      this.$auth.logout({
+        returnTo: `${window.location.origin}/logout`
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/auth/Logout.vue
@@ -0,0 +1,11 @@
+<template>
+<div></div>
+</template>
+<script>
+export default {
+  name: 'Logout',
+  mounted () {
+    this.$router.push({ path: '/' })
+  }
+}
+</script>
