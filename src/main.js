599535f11d850a8347a2790e785a12e59657a336
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2020-07-15 19:07:47 +0000

    setup Auth0

--- src/main.js
+++ src/main.js
@@ -5,6 +5,25 @@ import router from './router'
 import './assets/css/tailwind.css'
 import './assets/fonts/Inter/inter.css'
 
+// Import the Auth0 configuration
+import { domain, clientId } from '../auth_config.json'
+
+// Import the plugin here
+import { Auth0Plugin } from './utils/auth/index'
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    )
+  }
+})
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+import { authGuard } from '../utils/auth'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -11,12 +13,33 @@ const routes = [
     component: Home
   },
   {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/auth/Login.vue')
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    component: () => import(/* webpackChunkName: "logout" */ '../views/auth/Logout.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    beforeEnter: authGuard,
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/demo',
+    name: 'Demo',
+    component: () => import(/* webpackChunkName: "demo" */ '../views/Demo.vue')
   }
 ]
 
